32266ecaacbb27b3f3c886bf361993ed00164873
yusuf <yusufsaidcanbaz@gmail.com>
2017-11-30 17:33:46 +0300

    change files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
+var express = require("express");
+var app     = express();
+var path    = require("path");
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(__dirname));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.listen(PORT);
+
+console.log("Running at Port ", PORT);
